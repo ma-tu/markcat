c388fbd7dfe686080e6e33cd594b268fa0133d96
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-01-22 11:18:12 +0900

    setup gulp+typescript+babel

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject('tsconfig.json', {});
+
+gulp.task('scripts', function() {
+    var tsResult = tsProject.src() // instead of gulp.src(...)
+        .pipe(ts(tsProject))
+        .pipe(babel())
+        .pipe(gulp.dest('target'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.ts', ['scripts'])
+});
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+var abc = [1234]
+abc.toString
+console.log(abc.map(item => item * 2))
