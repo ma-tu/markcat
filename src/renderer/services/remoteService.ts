b03252d226d3fd62125d77d56dd2d89f5201fff0
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-02-28 11:42:42 +0900

    Fixed error at startup

--- src/renderer/services/remoteService.ts
+++ src/renderer/services/remoteService.ts
@@ -4,7 +4,7 @@ export function getArgumentInitPath(): string {
   const osXOpenFile = remote.getGlobal('openFilePath')
   const argv = remote.process.argv.concat([osXOpenFile])
   const initPage = argv[argv.length-1]
-  if ((argv.length == 2) && (initPage.toLowerCase().endsWith("md"))){
+  if ((argv.length == 2) && (initPage !== null)&& (initPage.toLowerCase().endsWith("md"))){
     return initPage
   }else{
     return ""
